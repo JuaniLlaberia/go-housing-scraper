ded529a0b6be717c853e99713b9c5482356bd806
JuaniLlaberia <juanillaberia2002@gmail.com>
2025-05-15 20:36:56 -0300

    Set up gemini

---
+++ configs/gemini.go
@@ -0,0 +1,92 @@
+package configs
+
+import (
+	"context"
+	"scraper/utils"
+
+	"google.golang.org/genai"
+)
+
+type GeminiConfig struct {
+	APIKey          string
+	ModelName       string
+	Temperature     *float32
+	TopP            *float32
+	TopK            *int32
+	MaxOutputTokens *int32
+	StopSequences   []string
+	SafeSettings    []*genai.SafetySetting
+}
+
+func DefaultSafetySettings() []*genai.SafetySetting {
+	return []*genai.SafetySetting{
+		{
+			Category:  genai.HarmCategoryHateSpeech,
+			Threshold: genai.HarmBlockThresholdBlockMediumAndAbove,
+		},
+		{
+			Category:  genai.HarmCategoryDangerousContent,
+			Threshold: genai.HarmBlockThresholdBlockMediumAndAbove,
+		},
+		{
+			Category:  genai.HarmCategoryHarassment,
+			Threshold: genai.HarmBlockThresholdBlockMediumAndAbove,
+		},
+		{
+			Category:  genai.HarmCategorySexuallyExplicit,
+			Threshold: genai.HarmBlockThresholdBlockMediumAndAbove,
+		},
+	}
+}
+
+func Gemini(contents []*genai.Content) (string, error) {
+	apiKey := utils.ProcessEnv("GEMINI_API_KEY")
+	ctx := context.Background()
+
+	client, err := genai.NewClient(ctx, &genai.ClientConfig{
+		APIKey:  apiKey,
+		Backend: genai.BackendGeminiAPI,
+	})
+	if err != nil {
+		return "", err
+	}
+
+	temp := float32(0.1)
+	topP := float32(0.3)
+	topK := float32(10.0)
+	maxOutputTokens := int32(8192)
+
+	const systemInstructions = `
+		You are a real state agent analyzing properties. Your task is to analyze certain properties and create analysis on them.
+
+		General Behavior Guidelines:
+		- Always return output in valid JSON.
+		- Do not include any explanatory text or commentary outside the JSON.
+		- When uncertain about missing context use default values based on the topics and prompt.
+		- If you encounter a repeated property you can remove it.
+		- If a property does not have a price, do not consider it.
+		- Focus on accuracy, precision when analysing.
+
+		Formatting Rules:
+		- Always wrap your entire output inside a valid JSON object or array depending on the endpoint requirements.
+		- Use snake_case for all keys.
+		`
+
+	config := &genai.GenerateContentConfig{
+		Temperature:       &temp,
+		TopP:              &topP,
+		TopK:              &topK,
+		MaxOutputTokens:   maxOutputTokens,
+		ResponseMIMEType:  "application/json",
+		SystemInstruction: genai.NewContentFromText(systemInstructions, genai.RoleUser),
+		SafetySettings:    DefaultSafetySettings(),
+	}
+
+	result, err := client.Models.GenerateContent(ctx, "gemini-2.0-flash", contents, config)
+
+	if err != nil {
+		return "", err
+	}
+
+	return result.Text(), nil
+}
--- go.mod
+++ go.mod
@@ -3,20 +3,43 @@ module scraper
 go 1.24.2
 
 require (
+	cloud.google.com/go v0.121.1 // indirect
+	cloud.google.com/go/auth v0.16.1 // indirect
+	cloud.google.com/go/compute/metadata v0.7.0 // indirect
 	github.com/PuerkitoBio/goquery v1.10.3 // indirect
 	github.com/andybalholm/cascadia v1.3.3 // indirect
 	github.com/antchfx/htmlquery v1.3.4 // indirect
 	github.com/antchfx/xmlquery v1.4.4 // indirect
 	github.com/antchfx/xpath v1.3.4 // indirect
+	github.com/felixge/httpsnoop v1.0.4 // indirect
+	github.com/go-logr/logr v1.4.2 // indirect
+	github.com/go-logr/stdr v1.2.2 // indirect
 	github.com/gobwas/glob v0.2.3 // indirect
 	github.com/gocolly/colly v1.2.0 // indirect
 	github.com/golang/groupcache v0.0.0-20241129210726-2c02b8208cf8 // indirect
 	github.com/golang/protobuf v1.5.4 // indirect
+	github.com/google/go-cmp v0.7.0 // indirect
+	github.com/google/s2a-go v0.1.9 // indirect
+	github.com/googleapis/enterprise-certificate-proxy v0.3.6 // indirect
+	github.com/googleapis/gax-go/v2 v2.14.2 // indirect
+	github.com/gorilla/websocket v1.5.3 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
 	github.com/kennygrant/sanitize v1.2.4 // indirect
 	github.com/saintfish/chardet v0.0.0-20230101081208-5e3ef4b5456d // indirect
 	github.com/temoto/robotstxt v1.1.2 // indirect
+	go.opencensus.io v0.24.0 // indirect
+	go.opentelemetry.io/auto/sdk v1.1.0 // indirect
+	go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp v0.60.0 // indirect
+	go.opentelemetry.io/otel v1.35.0 // indirect
+	go.opentelemetry.io/otel/metric v1.35.0 // indirect
+	go.opentelemetry.io/otel/trace v1.35.0 // indirect
+	golang.org/x/crypto v0.38.0 // indirect
 	golang.org/x/net v0.40.0 // indirect
+	golang.org/x/sys v0.33.0 // indirect
 	golang.org/x/text v0.25.0 // indirect
 	google.golang.org/appengine v1.6.8 // indirect
+	google.golang.org/genai v1.5.0 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20250512202823-5a2f75b736a9 // indirect
+	google.golang.org/grpc v1.72.1 // indirect
 	google.golang.org/protobuf v1.36.6 // indirect
 )
